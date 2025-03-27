d7f61150f0ada87d40a44f647c0f98abf2c19b19
RubensJr21 <junior.correa2001@gmail.com>
2025-03-27 15:08:53 -0300

    Atualiza valores usados como parâmetro na dimensão I2

--- v4.0/lib/dimensions/dimension_6_I2.py
+++ v4.0/lib/dimensions/dimension_6_I2.py
@@ -5,9 +5,14 @@ def I2(base_main: pd.DataFrame, base_familias: pd.DataFrame) -> pd.DataFrame:
 
     b_familias = base_familias.copy()
 
-    b_familias["bool_Banheiro_COMP"] = b_familias["21 - Existe banheiro de uso exclusivo, com chuveiro e vaso sanitário, neste domicílio, inclusive os localizados no terreno?*"].fillna('').eq('N')
+    b_familias["bool_Banheiro_COMP"] = b_familias["21 - Existe banheiro de uso exclusivo, com chuveiro e vaso sanitário, neste domicílio, inclusive os localizados no terreno?*"].fillna('').eq('Não')
     
-    values_for_opt_Escoamento = ["F", "V", "D", "O"]
+    values_for_opt_Escoamento = [
+        "Fossa rudimentar",
+        "Vala a céu aberto",
+        "Direto para um rio, lago ou mar",
+        "Outra forma"
+    ]
     b_familias["opt_Escoamento_COMP"] = b_familias["22 - De que forma é feito o escoamento do banheiro ou sanitário?"].fillna('').isin(values_for_opt_Escoamento)
 
     b_familias["bool_I2"] = b_familias["bool_Banheiro_COMP"] | b_familias["opt_Escoamento_COMP"]
