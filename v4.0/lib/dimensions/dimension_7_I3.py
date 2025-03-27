fc998f47bc41cd15c104a0b6502161b6b4ccdb0f
RubensJr21 <junior.correa2001@gmail.com>
2025-03-27 15:08:54 -0300

    Atualiza valores usados como parâmetro na dimensão I3

--- v4.0/lib/dimensions/dimension_7_I3.py
+++ v4.0/lib/dimensions/dimension_7_I3.py
@@ -7,20 +7,31 @@ def I3(base_main: pd.DataFrame, base_familias: pd.DataFrame) -> pd.DataFrame:
     b_familias["Test1"] = (
         b_familias["18 - Qual é a forma de iluminação utilizada no seu domicílio?"]
         .fillna('')
-        .isin(["G", "V", "O"])
+        .isin([
+            "Óleo, querosene ou gás",
+            "Vela",
+            "Outra forma"
+        ])
     )
 
     b_familias["Test2"] = (
         (
             b_familias["18 - Qual é a forma de iluminação utilizada no seu domicílio?"]
             .fillna('')
-            .isin(["M", "C", "S"])
+            .isin([
+                "Elétrica com medidor próprio",
+                "Elétrica com medidor comunitário",
+                "Elétrica sem medidor"
+            ])
         )
         &
         (
             b_familias["19 - Com que frequência a energia elétrica, proveniente de rede geral, está habitualmente disponível para este domicílio?"]
             .fillna('')
-            .isin(["H", "O"])
+            .isin([
+                "Diária, por algumas horas",
+                "Outra frequência" # Olhando no formulário do Kobo v4.0 ao invés de "Outra forma" é "Outra frequência"
+            ])
         )
     )
     
@@ -28,20 +39,30 @@ def I3(base_main: pd.DataFrame, base_familias: pd.DataFrame) -> pd.DataFrame:
        (
         b_familias["20 - Como são preparadas as refeições?*"]
         .fillna('')
-        .isin(["G", "B", "E"])
+        .isin([
+            "Outro (Fogareiro, fogueira, usa combustíveis como álcool, querosene, diesel, papelão, plástico etc.)", # No arquivo 'v4.0_Questionário_Multidimensional.xlsx' na planilha 'choices' onde o list_name é 'gc8qb78' linha 50 especificamente apresenta a opção de "Outro (Fogareiro, fogueira, usa combustíveis como álcool, querosene, diesel, papelão, plástico etc.)" que pode ser uma opção ao "Óleo, querosene ou gás"
+            "Fogão, com gás de botijão",
+            "Equipamentos elétricos  (Forno, Microondas, Fogão, Chapa, AirFryer etc)"
+        ])
     )
         &
         (
         b_familias["20.1 - A família é obrigada a utilizar outros combustíveis para preparar as refeições durante o mês por falta de dinheiro?"]
         .fillna('')
-        .isin(["S", "A"])
+        .isin([
+            "Sim, todo mês acontece",
+            "Sim, em alguns meses acontece"
+        ])
     )
     )
 
     b_familias["Test4"] = (
         b_familias["20 - Como são preparadas as refeições?*"]
         .fillna('')
-        .isin(["X", "O"])
+        .isin([
+            "Fogão a lenha rudimentar ou improvisado",
+            "Outro (Fogareiro, fogueira, usa combustíveis como álcool, querosene, diesel, papelão, plástico etc.)" # Opção "Outra forma" não consta no formulário v4.0. Única opção que pode se assemelhar: "Outro (Fogareiro, fogueira, usa combustíveis como álcool, querosene, diesel, papelão, plástico etc.)"
+        ])
     )
 
     b_familias["DIM_I3_Comp"] = (
