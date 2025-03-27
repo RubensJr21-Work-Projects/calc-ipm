5b2737a8c3486fa60bcebfa4e983c60cdb4cc93c
RubensJr21 <junior.correa2001@gmail.com>
2025-03-27 15:08:53 -0300

    Atualiza valores usados como parâmetro na dimensão E1

--- v4.0/lib/dimensions/dimension_3_E1.py
+++ v4.0/lib/dimensions/dimension_3_E1.py
@@ -7,27 +7,30 @@ def E1(base_main: pd.DataFrame, base_familias: pd.DataFrame, base_membros: pd.Da
     b_membros = base_membros.copy()
 
     # 1.
-    # 1.1 Verificar se M19 - Qual foi o curso mais elevado que frequentou, no qual concluiu pelo menos uma série/ano? != ["EJM", "SUP"] OR M19.1 - E qual foi a última série que foi aprovado? != "M3A"
+    # 1.1 Verificar se M19 != ["Ensino Médio EJA (Supletivo)", "Superior, Aperfeiçoamento, Especialização, Mestrado, Doutorado"] OR M19.1 != "3º ano (curso completo)"
     ## OBS: EDM Não estava na base de cáluclo enviada pelo Rafael
     ## OBS(Revisão pós reunião): Em conversa com Rafel, ele informou que o EDM não conta
     ##       "Pois quem responder EDM ainda estará estudando, e consequentemente não completou o ensino médio"
-    values_for_opt_educacao = ["EJM", "SUP"]
+    values_for_opt_educacao = [
+        "Ensino Médio EJA (Supletivo)",
+        "Superior, Aperfeiçoamento, Especialização, Mestrado, Doutorado"
+    ]
     b_membros["Não concluiu o ensino médio"] = (
         ~b_membros["M19 - Qual foi o curso mais elevado que frequentou, no qual concluiu pelo menos uma série/ano?"].isin(values_for_opt_educacao)
         |
-        ~b_membros["M19.1 - E qual foi a última série que foi aprovado?"].fillna("").eq("M3A")
+        ~b_membros["M19.1 - E qual foi a última série que foi aprovado?"].fillna("").eq("3º ano (curso completo)")
     )
     
     # 1.2 Verificar se 4 <= Idade <= 17
     b_membros["4 <= Idade <= 17"] = b_membros["Idade"].between(4, 17, inclusive="both")
 
-    # 1.3 Verificar se M16 - Frequenta regularmente a escola? (exclusiva para MENOR de 18 anos) == "N" OR M17 - Está em risco de exclusão escolar? (exclusiva para MENOR de 18 anos) == "N" OR M18 - Está matriculado na escola? (exclusiva para MENOR de 18 anos) == "N"
+    # 1.3 Verificar se M16 == "Não" OR M17 == "Sim" OR M18 == "Não"
     b_membros["Fora da escola ou em risco"] = (
-        b_membros["M16 - Frequenta regularmente a escola? (exclusiva para MENOR de 18 anos)"].fillna("").eq("N")
+        b_membros["M16 - Frequenta regularmente a escola? (exclusiva para MENOR de 18 anos)"].fillna("").eq("Não")
         |
-        b_membros["M17 - Está em risco de exclusão escolar? (exclusiva para MENOR de 18 anos)"].fillna("").eq("S") # Deveria ser S
+        b_membros["M17 - Está em risco de exclusão escolar? (exclusiva para MENOR de 18 anos)"].fillna("").eq("Sim") # Deveria ser S
         |
-        b_membros["M18 - Está matriculado na escola? (exclusiva para MENOR de 18 anos)"].fillna("").eq("N")
+        b_membros["M18 - Está matriculado na escola? (exclusiva para MENOR de 18 anos)"].fillna("").eq("Não")
     )
 
 
@@ -36,8 +39,8 @@ def E1(base_main: pd.DataFrame, base_familias: pd.DataFrame, base_membros: pd.Da
     # 2.2 Verificar se 18 <= Idade <= 24
     b_membros["18 <= Idade <= 24"] = b_membros["Idade"].between(18, 24, inclusive="both")
 
-    # 2.3 M15 - Está estudando? == "N"
-    b_membros["Não está estudando"] = b_membros["M15 - Está estudando?"].fillna("").eq("N")
+    # 2.3 M15 - Está estudando? == "Não"
+    b_membros["Não está estudando"] = b_membros["M15 - Está estudando?"].fillna("").eq("Não")
 
     b_membros["Test_1"] = (
         b_membros["Não concluiu o ensino médio"]
