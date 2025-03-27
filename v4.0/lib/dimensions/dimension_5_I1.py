848305b9f54f08fcd20a01a39f92f06ef658787e
RubensJr21 <junior.correa2001@gmail.com>
2025-03-27 15:08:53 -0300

    Atualiza valores usados como parâmetro na dimensão I1

--- v4.0/lib/dimensions/dimension_5_I1.py
+++ v4.0/lib/dimensions/dimension_5_I1.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def I1(base_main: pd.DataFrame, base_familias: pd.DataFrame) -> pd.DataFrame:
     b_familias = base_familias.copy()
-    b_familias["DIM_I1"] = b_familias.apply(lambda row: 0.05 if row["17.1 - Está a menos de 30m de caminhada, ida e volta?"] == "N" else 0, axis=1)
+    b_familias["DIM_I1"] = b_familias.apply(lambda row: 0.05 if row["17.1 - Está a menos de 30m de caminhada, ida e volta?"] == "Não" else 0, axis=1)
 
     return pd.merge(base_main, b_familias[["_id", "DIM_I1"]], on="_id", how="left")
