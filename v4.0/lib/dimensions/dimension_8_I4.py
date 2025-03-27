02fba4a8a832a0d0510416e2e7d5f826f3d4cf21
RubensJr21 <junior.correa2001@gmail.com>
2025-03-27 15:08:54 -0300

    Atualiza valores usados como parâmetro na dimensão I4

--- v4.0/lib/dimensions/dimension_8_I4.py
+++ v4.0/lib/dimensions/dimension_8_I4.py
@@ -4,14 +4,14 @@ import pandas as pd
 def I4(base_main: pd.DataFrame, base_membros: pd.DataFrame) -> pd.DataFrame:
     b_membros = base_membros.copy()
 
-    # Criando a coluna booleana que verifica se o Valor é 'N'
-    b_membros['M6 - Tem acesso à internet? == N'] = b_membros['M6 - Tem acesso à internet?'].fillna('').eq('N')  # True se for 'N', False caso contrário
+    # Criando a coluna booleana que verifica se o Valor é 'Não'
+    b_membros['M6 - Tem acesso à internet? == Não'] = b_membros['M6 - Tem acesso à internet?'].fillna('').eq('Não')  # True se for 'Não', False caso contrário
 
-    # Agrupando por Id_familia e verificando se todos os membros têm 'N'
-    df_resultado = b_membros.groupby('_id', as_index=False)['M6 - Tem acesso à internet? == N'].all().reset_index()
+    # Agrupando por Id_familia e verificando se todos os membros têm 'Não'
+    df_resultado = b_membros.groupby('_id', as_index=False)['M6 - Tem acesso à internet? == Não'].all().reset_index()
 
     # Renomeando a coluna final
-    df_resultado.rename(columns={'M6 - Tem acesso à internet? == N': 'Todos_membros_N'}, inplace=True)
+    df_resultado.rename(columns={'M6 - Tem acesso à internet? == Não': 'Todos_membros_N'}, inplace=True)
 
     df_resultado['DIM_I4'] = df_resultado['Todos_membros_N'].apply(lambda x: 0.05 if x else 0)
 
